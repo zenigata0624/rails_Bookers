44e5ab0fd90b8570a08f44f9e04a48dc3c3a42f7
tenyuikeda <124487739+zenigata0624@users.noreply.github.com>
2023-02-24 02:28:59 +0000

    [finish]bookers

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root :to => 'homes#top'
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show',as: 'book'
+  get 'books/:id/edit' => 'books#edit',as: 'edit_book'
+  patch 'books/:id' => 'books#update',as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
