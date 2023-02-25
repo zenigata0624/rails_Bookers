cc1dcec9a731aadac46c3f5649849633b4da660a
tenyuikeda <124487739+zenigata0624@users.noreply.github.com>
2023-02-25 03:05:25 +0000

    [finish]bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to => 'homes#top'
+  root to: 'homes#top'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show',as: 'book'
